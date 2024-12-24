7d77cb56675889d0fef2e779b62f4c9f6fa4d7e3
David Eliason <davethemaker@gmail.com>
2024-12-24 15:28:27 -0800

    code successfully creates VPC in Oregon region, subnet in AZ us-west-2a, IG, RT and routes pointing to IG, and security group

--- terraform_ec2_oneAZ/ec2.tf
+++ terraform_ec2_oneAZ/ec2.tf
@@ -1,11 +1,1 @@
-# Create an EC2 Instance
-resource "aws_instance" "tf_aws_ec2_2a" {
-  ami                    = "ami-07d9cf938edb0739b" # Replace with your desired AMI
-  instance_type          = "t2.micro"              # Replace with your desired instance type
-  subnet_id              = aws_subnet.tf_aws_subnet_2a.id
-  vpc_security_group_ids = [aws_security_group.tf_aws_sg_port22.id]
 
-  tags = {
-    Name = "tf_aws_ec2_us_w2a"
-  }
-}
