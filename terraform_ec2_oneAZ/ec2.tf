2655af864c70b404406cda68ebcda533bea3e505
David Eliason <davethemaker@gmail.com>
2024-12-24 16:40:25 -0800

    couldn't find location of private key of the pair so update code to create new key and place private in known folder

--- terraform_ec2_oneAZ/ec2.tf
+++ terraform_ec2_oneAZ/ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "Project1_ec2" {
   instance_type               = "t2.micro"              # Replace with your desired instance type
   subnet_id                   = aws_subnet.project1_subnet_us_west_2a.id
   vpc_security_group_ids      = [aws_security_group.project1_sg_port22.id]
-  key_name                    = "KeyPair"
+  key_name                    = aws_key_pair.Project1_key_pair.key_name
   associate_public_ip_address = true # Explicitly assign public IP
 
   tags = {
@@ -13,3 +13,8 @@ resource "aws_instance" "Project1_ec2" {
   }
 }
 
+# Key Pair
+resource "aws_key_pair" "Project1_key_pair" {
+  key_name   = "Project1_key_pair"       # Replace with your desired key pair name
+  public_key = file("~/.ssh/id_rsa.pub") # Path to your public key
+}
