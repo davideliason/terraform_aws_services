026d625e85078c67d4d04e9d74309d8b3d925937
David Eliason <davethemaker@gmail.com>
2024-12-25 08:22:43 -0800

    add default for vpc cidr

--- best_dir_vpc_sg_ec2/modules/vpc/variables.tf
+++ best_dir_vpc_sg_ec2/modules/vpc/variables.tf
@@ -1,5 +1,9 @@
 variable "vpc_name" {} # since no default given, this var is required- a value must be passed when calling the module or TF apply
-variable "cidr" {}
+variable "cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
 variable "azs" { default = [] } # an empty default is provided so no AZ used in one isn't provided
 variable "public_subnets" { default = [] }
 variable "private_subnets" { default = [] }
