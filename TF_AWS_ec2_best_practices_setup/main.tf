66ff5be17a74d9d923f19dfb337084a129cf7e43
David Eliason <davethemaker@gmail.com>
2024-12-24 21:33:46 -0800

    create vpc module using the official TF Registry AWS VPC Module for three AZ, nat gateway, and network ranges

--- TF_AWS_ec2_best_practices_setup/main.tf
+++ TF_AWS_ec2_best_practices_setup/main.tf
@@ -14,5 +14,33 @@ provider "aws" {
   profile                  = "vscodeaws"
   region                   = var.region
 }
+# placeholder
+resource "aws_vpc" "project1_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
+  tags = {
+    Name = "Project1VPC"
+  }
+}
+# end placeholder
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "project2-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Environment = "dev"
+    Project     = "project2"
+  }
+}
