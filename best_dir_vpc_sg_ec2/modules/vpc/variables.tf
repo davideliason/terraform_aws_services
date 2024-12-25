64b15c0b146ca9c3dfb3db50ec5a77c27620038a
David Eliason <davethemaker@gmail.com>
2024-12-25 05:00:33 -0800

    add note re vpc variable

--- best_dir_vpc_sg_ec2/modules/vpc/variables.tf
+++ best_dir_vpc_sg_ec2/modules/vpc/variables.tf
@@ -1,4 +1,4 @@
-variable "vpc_name" {}
+variable "vpc_name" {} # since no default given, this var is required- a value must be passed when calling the module or TF apply
 variable "cidr" {}
 variable "azs" { default = [] }
 variable "public_subnets" { default = [] }
