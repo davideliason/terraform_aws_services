462208eff7b605062fa0bffef1897f485bef2fd6
David Eliason <davethemaker@gmail.com>
2024-12-25 04:18:14 -0800

    use modules instead in main

--- TF_AWS_ec2_best_practices_setup/main.tf
+++ TF_AWS_ec2_best_practices_setup/main.tf
@@ -16,23 +16,27 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source          = "./modules/vpc"
+  vpc_name        = "my-vpc"
+  cidr            = "10.0.0.0/16"
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+  tags            = { Environment = "dev" }
+}
 
-  name = "project2-vpc"
-  cidr = "10.0.0.0/16"
+module "security_group" {
+  source = "./modules/security-group"
+  name   = "web-sg"
+  vpc_id = module.vpc.vpc_id
+  tags   = { Environment = "dev" }
+}
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
-    Environment = "dev"
-    Project     = "project2"
-  }
+module "ec2" {
+  source            = "./modules/ec2"
+  ami               = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI
+  instance_type     = "t2.micro"
+  subnet_id         = module.vpc.public_subnets[0]
+  security_group_id = module.security_group.security_group_id
+  tags              = { Environment = "dev" }
 }
