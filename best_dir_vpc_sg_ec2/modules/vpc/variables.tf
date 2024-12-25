6ecf3b032c7109004c1ffe9a18212aeab64a3aea
David Eliason <davethemaker@gmail.com>
2024-12-25 05:05:59 -0800

    add terraform.tfvars to explore another way of decoupling and providing var values

--- best_dir_vpc_sg_ec2/modules/vpc/variables.tf
+++ best_dir_vpc_sg_ec2/modules/vpc/variables.tf
@@ -1,6 +1,6 @@
 variable "vpc_name" {} # since no default given, this var is required- a value must be passed when calling the module or TF apply
 variable "cidr" {}
-variable "azs" { default = [] }
+variable "azs" { default = [] } # an empty default is provided so no AZ used in one isn't provided
 variable "public_subnets" { default = [] }
 variable "private_subnets" { default = [] }
 variable "tags" { default = {} }
