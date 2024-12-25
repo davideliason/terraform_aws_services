917e9ab45c416018cca92c28dbaf00408bef85a3
David Eliason <davethemaker@gmail.com>
2024-12-24 16:29:07 -0800

    the EC2 instance spins up with the key-pair referenced correctly, but there was no public IPv4 address so not able to ssh into it

--- terraform_ec2_oneAZ/ec2.tf
+++ terraform_ec2_oneAZ/ec2.tf
@@ -5,8 +5,10 @@ resource "aws_instance" "Project1_ec2" {
   instance_type          = "t2.micro"              # Replace with your desired instance type
   subnet_id              = aws_subnet.project1_subnet_us_west_2a.id
   vpc_security_group_ids = [aws_security_group.project1_sg_port22.id]
+  key_name               = "KeyPair"
 
   tags = {
     Name = "Project1EC2_2a"
   }
 }
+
