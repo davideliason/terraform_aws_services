6e4dd4ebb8cfb3dc499b18563db792c3a417b9b7
David Eliason <davethemaker@gmail.com>
2024-12-25 04:59:04 -0800

    successfully creates vps, sn, ec2 but no public ip address or key for acces.

--- best_dir_vpc_sg_ec2/main.tf
+++ best_dir_vpc_sg_ec2/main.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source          = "./modules/vpc"
   vpc_name        = "my-vpc"
   cidr            = "10.0.0.0/16"
-  azs             = ["us-east-1a", "us-east-1b"]
+  azs             = ["us-west-2a", "us-west-2b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
   tags            = { Environment = "dev" }
@@ -17,7 +17,7 @@ module "security_group" {
 
 module "ec2" {
   source            = "./modules/ec2"
-  ami               = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI
+  ami               = "ami-07d9cf938edb0739b" # Replace with a valid AMI
   instance_type     = "t2.micro"
   subnet_id         = module.vpc.public_subnets[0]
   security_group_id = module.security_group.security_group_id
